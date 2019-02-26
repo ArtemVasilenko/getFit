ea54089b5b10f2301ed0e19b227cf42d30890ca7
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-02-26 16:46:17 +0200

    Initial Commit

---
+++ getFit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  getFit
+//
+//  Created by Артем on 2/26/19.
+//  Copyright © 2019 Артем. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
