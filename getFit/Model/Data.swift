8e36a7fdc367634dbb1e6ba299a41a2a2645985b
ArtemVasilenko <artemvasilenko88@gmail.com>
2019-02-27 19:34:33 +0200

    1

---
+++ getFit/Model/Data.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+struct GlobalArr {
+    var globalArr = [""]
+}
+
+class A {
+    var arr = ["a1", "a2"]
+}
+
+class B {
+    var arr = ["b1", "b2", "b3"]
+}
+
+class C {
+    var arr = ["c1", "c2", "c3", "c4"]
+}
